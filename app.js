3188c531991ad9d3db7635a7eb2ec0c298caf5e8
Sameer Verma <71380483+Sameer1408@users.noreply.github.com>
2023-01-29 23:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://luminous-scone-113fa7.netlify.app/",
         methods:["GET","POST"],
     }
 })
